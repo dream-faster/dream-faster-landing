90f687ad0a7c6cee893b1685af7465244a7217d8
Daniel Szemerey <account@szemerey.eu>
2022-10-14 11:42:05 +0200

    feat: Updated page description.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -97,8 +97,8 @@ const Index = () => {
     <Main
       meta={
         <Meta
-          title="Next.js Boilerplate Presentation"
-          description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
+          title="Dream Faster AI Studio"
+          description="Independent R&D studio specialized in Artificial Intelligence."
         />
       }
     >
