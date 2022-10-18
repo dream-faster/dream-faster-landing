b121254d6d770456c686923ac28a0ae47ad1e322
Daniel Szemerey <account@szemerey.eu>
2022-10-18 11:02:33 +0200

    fix: Wide property.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,7 +2,10 @@ import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
 const About = () => (
-  <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
+  <Main
+    wide={true}
+    meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}
+  >
     <p>
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
       recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
