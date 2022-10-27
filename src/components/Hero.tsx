ac0d70f8410c7b61d5e4a9c4822cd4250571e3c1
Daniel Szemerey <account@szemerey.eu>
2022-10-27 16:30:05 +0200

    feat: Updated typescript name declration.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -39,7 +39,7 @@ const userInfo = [
   },
 ];
 
-const TinyLink = ({ url, name }: { url: string; name: string }) => (
+const TinyLink = ({ url, name }: { url?: string; name?: string }) => (
   <a
     className=" border-none text-yellow-400"
     href={url}
@@ -83,28 +83,27 @@ const ProfilePic = ({
 );
 
 export const Hero = () => (
-  <div className="flex h-full flex-col justify-between md:min-h-[calc(100vh-16rem)]">
+  <div className="flex h-full w-full flex-col justify-between md:min-h-[calc(100vh-16rem)]">
     <div className="absolute z-10 ml-[18px] h-5 w-1 bg-black outline-2 outline-white dark:bg-white md:ml-[-2px] " />
-    <div className="flex h-full flex-col justify-between">
-      {/* <p className=" h-full px-12 font-mono text-sm text-black dark:text-slate-300"> */}
-      <article className=" prose prose-zinc h-full px-12 font-mono text-sm text-black dark:prose-invert dark:text-slate-300">
+    <div className="flex h-full w-full flex-col justify-between">
+      <article className=" prose prose-zinc h-full w-full max-w-2xl px-12 font-mono text-sm text-black dark:prose-invert dark:text-slate-300">
+        <h2>Hi 👋</h2>
         We are an independent Machine Learning Research and Development Studio
         based in Berlin.
         <br />
         <br />
-        🏔️ We aspire to:
-        <br />
-        &emsp;- make state of the art algorithms more convenient to access
-        <br />
-        &emsp;- explore if ML practices hold up to scrutiny
-        <br />
-        &emsp;- work with experts to develop useful tools and solutions to
-        real-world problems
-        <br />
-        &emsp;- revitalize forgotten ideas in AI
-        <br />
-        &emsp;- create interpretable ML to expand human knowledge
-        {/* </p> */}
+        <b>🏔️ We aspire to:</b>
+        <p className="ml-2 w-full">
+          * make state of the art algorithms more convenient to access
+          <br />
+          * explore if ML practices hold up to scrutiny
+          <br />
+          * work with experts to develop useful tools and solutions to
+          real-world problems
+          <br />
+          * revitalize forgotten ideas in AI
+          <br />* create interpretable ML to expand human knowledge
+        </p>
       </article>
       <div className="mt-6 flex h-full flex-col items-start justify-start px-12">
         {userInfo.map((user, i) => (
