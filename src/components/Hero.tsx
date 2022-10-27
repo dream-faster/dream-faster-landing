b5b402e46f4726ebd03ac0490863ae52929f463e
Daniel Szemerey <account@szemerey.eu>
2022-10-27 16:18:41 +0200

    feat: Content changed.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -86,12 +86,13 @@ export const Hero = () => (
   <div className="flex h-full flex-col justify-between md:min-h-[calc(100vh-16rem)]">
     <div className="absolute z-10 ml-[18px] h-5 w-1 bg-black outline-2 outline-white dark:bg-white md:ml-[-2px] " />
     <div className="flex h-full flex-col justify-between">
-      <p className=" h-full px-12 font-mono text-sm text-black dark:text-slate-300">
+      {/* <p className=" h-full px-12 font-mono text-sm text-black dark:text-slate-300"> */}
+      <article className=" prose prose-zinc h-full px-12 font-mono text-sm text-black dark:prose-invert dark:text-slate-300">
         We are an independent Machine Learning Research and Development Studio
         based in Berlin.
         <br />
         <br />
-        We love to:
+        🏔️ We aspire to:
         <br />
         &emsp;- make state of the art algorithms more convenient to access
         <br />
@@ -99,7 +100,12 @@ export const Hero = () => (
         <br />
         &emsp;- work with experts to develop useful tools and solutions to
         real-world problems
-      </p>
+        <br />
+        &emsp;- revitalize forgotten ideas in AI
+        <br />
+        &emsp;- create interpretable ML to expand human knowledge
+        {/* </p> */}
+      </article>
       <div className="mt-6 flex h-full flex-col items-start justify-start px-12">
         {userInfo.map((user, i) => (
           <ProfilePic
