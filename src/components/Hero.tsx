bcb58ca9e834677fe778494f8a5c9d5066d0ce3b
Daniel Szemerey <account@szemerey.eu>
2022-10-27 22:36:28 +0200

    feat: Better factoring.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,8 @@ export const Hero = () => (
     <div className="flex h-full w-full flex-col justify-between">
       <article className=" prose prose-zinc h-full w-full max-w-2xl px-12 font-mono text-sm text-black dark:prose-invert dark:text-slate-300">
         <h2>Welcome 👋</h2>
-        <p className="underline decoration-yellow-400 underline-offset-2">
+        {/* <p className="underline decoration-yellow-400 underline-offset-2"> */}
+        <p>
           <b>
             We are an independent Machine Learning Research and Development
             Studio based in Berlin.
@@ -15,27 +16,28 @@ export const Hero = () => (
         </p>
       </article>
 
-      <article className="prose prose-zinc h-full w-full max-w-2xl px-12 font-mono text-sm text-black opacity-50 dark:prose-invert dark:text-slate-300">
-        <br />
-        <b>We aspire to:</b>
+      <article className="prose prose-zinc h-full w-full max-w-2xl px-12 font-mono text-sm text-black opacity-60 dark:prose-invert dark:text-slate-300">
         <p className=" w-full whitespace-pre-line">
+          {`\n`}We aspire to:{`\n`}
           {[
             'Make state of the art algorithms more convenient to access',
             'Explore if ML practices hold up to scrutiny',
             'Work with experts to develop useful tools and solutions to real-world problems',
             'Revitalize forgotten ideas in AI',
             'Create interpretable ML to expand human knowledge',
-          ].map((e) => `🟨 ${e}\n`)}
+          ].map((e) => `* ${e}\n`)}
         </p>
       </article>
     </div>
-    <div className="mt-6 flex flex-col flex-wrap px-12 md:flex-row">
-      <ProfileCardSection />
-      <CommunityCardSection />
-    </div>
-    <div className="-ml-2 px-12">
-      <MajorButton text="Who we are" link="/about" />
-      <MajorButton text="Collaborate with us" link="/collaborate" primary />
+    <div>
+      <div className="mt-6 flex flex-col flex-wrap px-12 md:flex-row">
+        <ProfileCardSection />
+        <CommunityCardSection />
+      </div>
+      <div className="-ml-2 px-12">
+        <MajorButton text="Who we are" link="/about" />
+        <MajorButton text="Collaborate with us" link="/collaborate" primary />
+      </div>
     </div>
   </div>
 );
