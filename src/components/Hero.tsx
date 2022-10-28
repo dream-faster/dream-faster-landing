bd4311e0983ebaf98310f887fc854c045c734a29
Daniel Szemerey <account@szemerey.eu>
2022-10-28 10:15:05 +0200

    Feature(Content): New hero (#10)
    * feat: New hero content.
    
    * feat: Added community part.
    
    * feat: Better factoring.
    
    * chore: Taking out community for now.
    
    * chore: took out link for the time being.
    
    * chore: Maybe if my is 1px (there might be an overlap?)
    
    * fix: Safari border issue.
    
    * fix: Safari border issue.
    
    * feat: Shorter logo on mobile.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,29 +6,38 @@ export const Hero = () => (
     <div className="absolute z-10 ml-[18px] h-5 w-1 bg-black outline-2 outline-white dark:bg-white md:ml-[-2px] " />
     <div className="flex h-full w-full flex-col justify-between">
       <article className=" prose prose-zinc h-full w-full max-w-2xl px-12 font-mono text-sm text-black dark:prose-invert dark:text-slate-300">
-        <h2>Hi 👋</h2>
-        We are an independent Machine Learning Research and Development Studio
-        based in Berlin.
-        <br />
-        <br />
-        <b>We aspire to:</b>
-        <p className="ml-2 w-full">
-          * make state of the art algorithms more convenient to access
-          <br />
-          * explore if ML practices hold up to scrutiny
-          <br />
-          * work with experts to develop useful tools and solutions to
-          real-world problems
-          <br />
-          * revitalize forgotten ideas in AI
-          <br />* create interpretable ML to expand human knowledge
+        <h2>Welcome 👋</h2>
+        {/* <p className="underline decoration-yellow-400 underline-offset-2"> */}
+        <p>
+          <b>
+            We are an independent Machine Learning Research and Development
+            Studio based in Berlin.
+          </b>
         </p>
       </article>
-      <ProfileCardSection />
+
+      <article className="prose prose-zinc h-full w-full max-w-2xl px-12 font-mono text-sm text-black opacity-60 dark:prose-invert dark:text-slate-300">
+        <p className=" w-full whitespace-pre-line">
+          {`\n`}We aspire to:{`\n`}
+          {[
+            'Make state of the art algorithms more convenient to access',
+            'Explore if ML practices hold up to scrutiny',
+            'Work with experts to develop useful tools and solutions to real-world problems',
+            'Revitalize forgotten ideas in AI',
+            'Create interpretable ML to expand human knowledge',
+          ].map((e) => `* ${e}\n`)}
+        </p>
+      </article>
     </div>
-    <div className="px-12">
-      <MajorButton text="Who we are" link="/about" />
-      <MajorButton text="Collaborate with us" link="/collaborate" primary />
+    <div>
+      <div className="mt-6 flex flex-col flex-wrap px-12 md:flex-row">
+        <ProfileCardSection />
+        {/* <CommunityCardSection /> */}
+      </div>
+      <div className="-ml-2 px-12">
+        <MajorButton text="Who we are" link="/about" />
+        <MajorButton text="Collaborate with us" link="/collaborate" primary />
+      </div>
     </div>
   </div>
 );
